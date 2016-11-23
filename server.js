d17626daee5d78c4f2a3a56dfdba73e4064b0343
James Quiambao <james.quiambao@yahoo.ca>
2016-11-23 22:26:29 +0000

    Setting up Unix Route

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ var express = require('express');
 
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/', function(req,res){
+  res.send("Hello World!");
+})
+
+//Unix
+app.get('/:id', function (req, res) {
+  var object = {
+    'unix': req.params.id
+  }
+  res.send(JSON.stringify(object)); 
 }); 
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-});
+app.listen(8080); 
