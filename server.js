51443b49f75c887f800ba4309fbf5f21371b25dc
James Quiambao <james.quiambao@yahoo.ca>
2016-11-24 05:39:38 +0000

    Unix case solved for now

--- server.js
+++ server.js
@@ -3,35 +3,27 @@ var app = express();
 var moment = require("moment"); 
 
 app.get('/', function(req,res){
-  res.send("Hello World!");
+  res.send("TimeStamp App!");
 })
+
 
 
 app.get('/:id', function (req, res) {
   
   //Unix
-  var object = {
-    'unix': Number(req.params.id),
-    'natural': moment.unix(Number(req.params.id)).format('MMMM Do YYYY')
-  }
+  // var object = {
+  //   'unix': Number(req.params.id),
+  //   'natural': moment.unix(Number(req.params.id)).format('MMMM Do YYYY')
+  // }
   
   //Natural Language 
-    // var object = {
-    //   'unix': moment().unix(),
-    //   'natural': decodeURIComponent(req.params.id)
-    // }
+    var object = {
+      'unix': moment(decodeURIComponent(req.params.id)).format('X'),
+      'natural': decodeURIComponent(req.params.id)
+    }
   
   res.send(JSON.stringify(object)); 
+  console.log(moment(req.params.id)); 
 }); 
-
-//Natural Language
-// app.get('/:id', function (req, res){
-//     var object = {
-//       'natural': decodeURIComponent(req.params.id)
-//     }
-//     res.send(JSON.stringify(object)); 
-// });
-
-
 
 app.listen(8080); 
