a22878dfa820437233e52d4fcdbb2b8d304d4ed0
James Quiambao <james.quiambao@yahoo.ca>
2016-11-24 04:59:37 +0000

    decoded url

--- server.js
+++ server.js
@@ -6,21 +6,32 @@ app.get('/', function(req,res){
   res.send("Hello World!");
 })
 
-//Unix
+
 app.get('/:id', function (req, res) {
+  
+  //Unix
   var object = {
     'unix': Number(req.params.id),
     'natural': moment.unix(Number(req.params.id)).format('MMMM Do YYYY')
   }
+  
+  //Natural Language 
+    // var object = {
+    //   'unix': moment().unix(),
+    //   'natural': decodeURIComponent(req.params.id)
+    // }
+  
   res.send(JSON.stringify(object)); 
 }); 
 
 //Natural Language
-app.get('/:id', function (req, res){
-    var object = {
-      'natural': req.params.id
-    }
-    res.send(JSON.stringify(object)); 
-}); 
+// app.get('/:id', function (req, res){
+//     var object = {
+//       'natural': decodeURIComponent(req.params.id)
+//     }
+//     res.send(JSON.stringify(object)); 
+// });
+
+
 
 app.listen(8080); 
