59177a4ff694138ce2882e48efd637aada522d9e
James Quiambao <james.quiambao@yahoo.ca>
2016-11-24 04:19:02 +0000

    Added npm moment.js and processed unix

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 var express = require('express'); 
-
-var app = express()
+var app = express(); 
+var moment = require("moment"); 
 
 app.get('/', function(req,res){
   res.send("Hello World!");
 })
 
 //Unix
-// app.get('/:id', function (req, res) {
-//   var object = {
-//     'unix': Number(req.params.id)
-//   }
-//   res.send(JSON.stringify(object)); 
-// }); 
+app.get('/:id', function (req, res) {
+  var object = {
+    'unix': Number(req.params.id),
+    'natural': moment.unix(Number(req.params.id)).format('MMMM Do YYYY')
+  }
+  res.send(JSON.stringify(object)); 
+}); 
 
 //Natural Language
 app.get('/:id', function (req, res){
